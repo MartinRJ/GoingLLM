84f0f355b29d055424502a364d8ae8481aaba317
MartinRJ <fuerholz@gmx.net>
2023-03-10 03:01:40 +0100

    Bug in debug output

--- goingllm.py
+++ goingllm.py
@@ -338,12 +338,12 @@ def truncate_string_to_tokens(string, num_tokens):
     length = len(tokens)
 
     if length > num_tokens:
-        print("Length: " + length + " tokens. Too long, truncating to " + num_tokens, flush=True)
+        print("Length: " + str(length) + " tokens. Too long, truncating to " + str(num_tokens), flush=True)
         truncated_tokens = tokens[:num_tokens] # truncate the tokens if they exceed the maximum
         truncated_string = enc.decode(truncated_tokens) # decode the truncated tokens
         return truncated_string
     else:
-        print("Length: " + length + " tokens. Resuming.", flush=True)
+        print("Length: " + str(length) + " tokens. Resuming.", flush=True)
         return string
 
 def yes_or_no(string):
