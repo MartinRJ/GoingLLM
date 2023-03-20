11d31eaf4400d0a85c75b815ed3741b760bf26ca
MartinRJ <fuerholz@gmx.net>
2023-03-20 04:21:06 +0100

    Refining final prompt without google search

--- goingllm.py
+++ goingllm.py
@@ -207,7 +207,7 @@ def generate_final_result_without_search(usertask, task_id):
 
 def generate_final_response_without_search_results(usertask, task_id):
     #Make a regular query
-    system_prompt = "Ich bin dein persönlicher Assistent für die Internetrecherche, und antworte gerade ohne Internetrecherche, da ich zuvor entschieden habe, dass die Anfrage keine Internetrecherche benötigte."
+    system_prompt = "Ich bin dein persönlicher Assistent für die Internetrecherche, und antworte gerade ohne Internetrecherche, da ich zuvor entschieden habe, dass die Anfrage keine Internetrecherche benötigt."
     usertask = truncate_string_to_tokens(usertask, MAX_TOKENS_FINAL_RESULT, system_prompt)
     final_result = chatcompletion(system_prompt, usertask, TEMPERATURE_FINAL_RESULT, MAX_TOKENS_FINAL_RESULT, task_id)
     return final_result
