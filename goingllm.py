b895e4b797746a73cfbc5e08ec81a34df7e3ab83
MartinRJ <fuerholz@gmx.net>
2023-03-31 13:40:50 +0200

    Improved debugoutput in chatcompletion

--- goingllm.py
+++ goingllm.py
@@ -824,7 +824,8 @@ def chatcompletion(system_prompt, prompt, completiontemperature, completionmaxto
                     {"role": "assistant", "content": assistantmessage},
                     {"role": "user", "content": prompt2}
                 ]
-            debuglog(f"chatcompletion - assistantmessage:\n{assistantmessage}\nchatcompletion - prompt2:\n{prompt2}")
+            debuglog(f"chatcompletion - assistantmessage:\n{assistantmessage}")
+            debuglog(f"chatcompletion - prompt2:\n{prompt2}")
         response = openai.ChatCompletion.create(
         model=MODEL,
         temperature=completiontemperature,
